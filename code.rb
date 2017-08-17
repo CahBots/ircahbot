af671dee0836a8df13ee8625a80dd3cc95f4836f
2003cah <2003cah@gmail.com>
2017-08-17 19:12:07 -0400

    First commit I guess, let's pray I can get some modified test code running

---
+++ code.rb
@@ -0,0 +1,19 @@
+require 'cinch'
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.cahbot.pro"
+    c.channels = ["#general", "#cah"]
+    c.nick = "IRCahBot"
+  end
+
+  on :message, "^help" do |m|
+    m.reply "Hey #{m.user.nick}, this is IRCahBot, the IRC version of a bot that was originally for Discord. Currently the only commands are the one you just used and ^ping"
+  end
+  
+  on :message, "^ping" do |m|
+    m.reply "Pong! That took #{((Time.now - m.time) * 1000).to_i}ms"
+  end
+end
+
+bot.start
