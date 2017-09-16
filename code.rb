c64c63029860d8a1d1adfd40eb82dd689dfc0141
Chewsterchew <chew@chew.pw>
2017-09-16 10:51:19 -0500

    make bot join ircahbot channel on startup

--- code.rb
+++ code.rb
@@ -5,7 +5,7 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.cahbot.pro"
-    c.channels = ["#general", "#cah"]
+    c.channels = ["#general", "#cah","#ircahbot"]
     c.nick = "IRCahBot"
     c.user = "IRCahBot"
     c.realname = "IRCahBot"
