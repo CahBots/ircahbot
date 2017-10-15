65095b44b9e7901232c0665d5b1a3aaee9ca06c2
Christopher Holland <2003cah@gmail.com>
2017-10-14 20:47:18 -0400

    when gitignores don't really ignore files

--- config.rb
+++
@@ -1,6 +0,0 @@
-configatron.nick = "IRCahBot"
-configatron.user = "IRCahBot"
-configatron.realname = "IRCahBot - the cool bean"
-configatron.channels = ["#general", "#cah","#ircahbot", "#bots", "#bot-testing"]
-configatron.server = "irc.cahbot.pro"
-configatron.nspass = "TheScrimmyBingus"
