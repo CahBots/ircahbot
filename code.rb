e5e0cff56a1d92c6833a9635f2c13e542afc4298
2003cah <2003cah@gmail.com>
2017-10-14 20:50:20 -0400

    I mean (try count is still in effect, 5, 195)

--- code.rb
+++ code.rb
@@ -5,11 +5,11 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = configatron.server
-    c.channels = configatron.channels
-    c.nick = configatron.nick
-    c.user = configatron.user
-    c.realname = configatron.realname
+    c.server = configatron.server.to_s
+    c.channels = configatron.channels.to_s
+    c.nick = configatron.nick.to_s
+    c.user = configatron.user.to_s
+    c.realname = configatron.realname.to_s
     c.plugins.plugins = [NS, JoinChannel, PartChannel, Eval, Help, Ping, Restart]
     c.plugins.prefix = /^\^/
   end
