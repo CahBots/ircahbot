e9122e8f48761fd61d7a4a8a28f09c57a0a97577
2003cah <2003cah@gmail.com>
2017-10-14 21:12:12 -0400

    get over here

--- code.rb
+++ code.rb
@@ -7,7 +7,7 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "#{configatron.server}"
-    c.channels = "#{configatron.channels}"
+    c.channels = configatron.channels
     c.nick = "#{configatron.nick}"
     c.user = "#{configatron.user}"
     c.realname = "#{configatron.realname}"
