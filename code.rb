290356763c049a390224aa39f464dc71ea81c2bc
Chewsterchew <chew@chew.pw>
2017-10-16 15:47:59 -0500

    Using to_s instead of string interpolation

--- code.rb
+++ code.rb
@@ -6,11 +6,11 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = "#{configatron.server}"
+    c.server = configatron.server.to_s
     c.channels = configatron.channels
-    c.nick = "#{configatron.nick}"
-    c.user = "#{configatron.user}"
-    c.realname = "#{configatron.realname}"
+    c.nick = configatron.nick.to_s
+    c.user = configatron.user.to_s
+    c.realname = configatron.realname.to_s
     c.plugins.plugins = [NS, JoinChannel, PartChannel, Eval, Help, Ping, Restart]
     c.plugins.prefix = /^\^/
   end
