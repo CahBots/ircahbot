a9d7dee203672552fc88b589b96ef48ccc5810d6
Chewsterchew <chew@chew.pw>
2017-10-16 15:49:37 -0500

    Use single quotes instead of double

--- plugins/partchannel.rb
+++ plugins/partchannel.rb
@@ -4,12 +4,12 @@ class PartChannel
   match /part (.+)/, method: :part
 
   def part(m, part)
-    if m.user.host == "king-of.meme"
-      if m.user.name == "Cah"
+    if m.user.host == 'king-of.meme'
+      if m.user.name == 'Cah'
         Channel(part).part
-        m.reply "Left the channel successfully!"
+        m.reply 'Left the channel successfully!'
       else
-        m.reply "You are not permitted to do this action!"
+        m.reply 'You are not permitted to do this action!'
       end
     end
   end
