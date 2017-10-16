1e78186886d4344091153ca3d4851d11ae1ec23e
Chewsterchew <chew@chew.pw>
2017-10-16 15:49:04 -0500

    Using single quotes instead of double

--- plugins/joinchannel.rb
+++ plugins/joinchannel.rb
@@ -4,12 +4,12 @@ class JoinChannel
   match /join (.+)/, method: :join
 
   def join(m, join)
-    if m.user.host == "king-of.meme"
-      if m.user.name == "Cah"
+    if m.user.host == 'king-of.meme'
+      if m.user.name == 'Cah'
         Channel(join).join
-        m.reply "Joined the channel successfully!"
+        m.reply 'Joined the channel successfully!'
       else
-        m.reply "You are not permitted to do this action!"
+        m.reply 'You are not permitted to do this action!'
       end
     end
   end
