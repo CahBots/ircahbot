942846870dfa7fec98077e12f2f95712450a9ecc
2003cah <2003cah@gmail.com>
2017-10-17 16:47:07 -0400

    FIX THE BUILD

--- plugins/quote.rb
+++ plugins/quote.rb
@@ -4,10 +4,11 @@ class RQuote
   match /rquote/, method: :rquote
 
   def rquote(m)
-    response = RestClient.get(configatron.quoteuri, header= { 'X-Mashape-Key' => configatron.quotekey })
+    response = RestClient.get(configatron.quoteuri, header = { 'X-Mashape-Key' => configatron.quotekey })
     jparse = JSON.parse(response)
     m.reply 'Quotes courtesy of https://market.mashape.com/andruxnet/random-famous-quotes#'
     m.reply "Quote: #{jparse.fetch('quote')}"
     m.reply "From: #{jparse.fetch('author')}"
   end
 end
+
