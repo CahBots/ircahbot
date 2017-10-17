b06e2b1756c7778f39f784b1e28cc7883b3c2c53
Chewsterchew <chew@chew.pw>
2017-10-17 15:51:43 -0500

    Pretty sure I said SITE FILES

--- plugins/esrever.rb
+++
@@ -1,9 +0,0 @@
-class Reverse
-  include Cinch::Plugin
-
-  match /reverse(.+)/, method: :execute
-
-  def execute(m, words)
-    m.reply words.to_s.reverse
-  end
-end
--- restart.sh
+++
@@ -1,9 +0,0 @@
-echo "Pulling that stable code"
-git pull origin master
-
-echo "Updating dependencies"
-bundle update
-
-echo "Booting up the bot"
-bundle exec ruby code.rb
-
