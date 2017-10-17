c6d17e2d127e30f48f6eaad00dfa11718eddcb77
2003cah <2003cah@gmail.com>
2017-10-17 16:44:45 -0400

    the S that ruined everything

--- plugins/quote.rb
+++ plugins/quote.rb
@@ -4,11 +4,7 @@ class RQuote
   match /rquote/, method: :rquote
 
   def rquote(m)
-<<<<<<< HEAD
     response = RestClient.get(configatron.quoteuri, header= { 'X-Mashape-Key' => configatron.quotekey })
-=======
-    response = RestClient.get(configatron.quoteuri, header = { 'X-Mashape-Key' => sconfigatron.quotekey })
->>>>>>> 70abcea926bc503f686151e2c84321ab52047931
     jparse = JSON.parse(response)
     m.reply 'Quotes courtesy of https://market.mashape.com/andruxnet/random-famous-quotes#'
     m.reply "Quote: #{jparse.fetch('quote')}"
