d5e48ad86b76ebe7b2b88ce24eb02027f1538216
Advaith <advaith1@users.noreply.github.com>
2017-11-05 18:23:40 -0800

    oops wrong quotes

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -4,6 +4,6 @@ class Flip
   match /flip/, method: :flip
 
   def flip(m)
-    m.reply ["woahdude, you got **Heads**", "woahdude, you got **Tails**", "You got **heads**", "You got **tails**"].sample
+    m.reply ['woahdude, you got **Heads**', 'woahdude, you got **Tails**', 'You got **heads**', 'You got **tails**'].sample
   end
 end
