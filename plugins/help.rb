f15ee9d999384359aff2bfc10b4dea73772a0d04
Chewsterchew <chew@chew.pw>
2017-11-09 21:11:43 -0600

    dont worry cah, i PROMISE this isnt on master this time

--- plugins/help.rb
+++
@@ -1,9 +0,0 @@
-class Help
-  include Cinch::Plugin
-
-  match /help/, method: :help
-
-  def help(m)
-    m.reply "Hey #{m.user.nick}, this is IRCahBot, the IRC version of a bot that was originally for Discord. Currently the only commands are the one you just used and ^ping"
-  end
-end
--- plugins/ns.rb
+++
@@ -1,9 +0,0 @@
-class NS
-  include Cinch::Plugin
-
-  listen_to :connect, method: :thing
-
-  def thing(_chungus)
-    User('NickServ').send("identify #{configatron.nspass}")
-  end
-end
